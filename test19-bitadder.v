d89ed0dcc29034d88bed89424826ae5d6680f060
Asmi <107591325+asmisawant@users.noreply.github.com>
2024-07-25 23:16:32 -0700

    Update test19-bitadder.v

--- test19-bitadder.v
+++ test19-bitadder.v
@@ -14,10 +14,15 @@ module tb_adder_19bit;
     initial begin
         $monitor("Time = %0d, a = %b, b = %b, sum = %b, carry_out = %b", $time, a, b, sum, carry_out);
 
+        //test cases with two's complement signed numbers
         a = 19'b0000000000000000001; b = 19'b0000000000000000001; #10;
         a = 19'b1111111111111111111; b = 19'b0000000000000000001; #10;
         a = 19'b1010101010101010101; b = 19'b0101010101010101010; #10;
         a = 19'b1111111111111111111; b = 19'b1111111111111111111; #10;
+
+        //test negative numbers
+        a = 19'sb1111111111111111110; b = 19'sb0000000000000000001; #10; // -2 + 1
+
 
         $finish;
     end
