dd39f7596031ebe05082dbf60a6216cf7b0c9f62
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-07-26 22:54:16 -0700

    Create mux_shift.v

---
+++ mux_shift.v
@@ -0,0 +1,37 @@
+module mux_shift(
+    input wire clk,
+    input wire rst,
+    input wire lr_clk,
+    input wire last_shift,
+    input wire sum_res,
+    output reg out
+
+); 
+reg data = 1'b0;
+
+reg prev_lr_clk;
+
+always @(posedge lr_clk) begin
+        //load sum into data
+        data <= sum_res; 
+end 
+
+always @(posedge clk) begin
+    if (! (lr_clk&& !prev_lr_clk)) begin
+        data <= last_shift; 
+    end
+    
+
+    prev_lr_clk <= lr_clk;
+end
+
+always @(negedge clk) begin
+// Send out a bit to out
+    out<=data; // output data held in data
+    
+end
+
+
+
+
+endmodule
