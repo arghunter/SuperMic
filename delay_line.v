c787646f3d9a3ab6668491941ba8cdfb1151d828
Sanjay Ravishankar <sanrav2016@gmail.com>
2024-07-28 17:10:09 -0400

    delay module

--- delay_line.v
+++ delay_line.v
@@ -1,27 +1,28 @@
-module delay_line #( 
-	parameter DELAY = 30	// Delay clock cycles
-)(
+module delay_line (
 	input wire clk,
 	input wire rst,
+        input wire [5:0] delay,
 	input wire [18:0] pcm_data,
 	output wire [18:0] delayed_pcm_data
 );
 
+parameter MAX_DELAY = 64;
 integer i, counter = 0;
-reg [18:0] buffer [DELAY:0];
+reg [18:0] buffer [MAX_DELAY:0];
 
 always @(posedge clk or posedge rst) begin
 	if (rst) begin
 		counter <= 0;
-		for (i = 0; i < DELAY + 1; i = i + 1) begin
+		for (i = 0; i <= MAX_DELAY; i = i + 1) begin
 			buffer[i] <= 19'b0;
 		end
 	end else begin
 		buffer[counter] <= pcm_data;
-		counter <= (counter + 1) % (DELAY + 1);
+		counter <= (counter + 1) % (delay + 1);
 	end
 end
 
-assign delayed_pcm_data = buffer[(counter + 1) % (DELAY + 1)];
+assign delayed_pcm_data = buffer[(counter + 1) % (delay + 1)];
 
 endmodule
+
---
+++ delay_module.v
@@ -0,0 +1,39 @@
+module delay_module (
+  input wire clk,
+  input wire rst,
+  input wire [4:0] delay_select
+);
+reg [18:0] pcm_data [15:0];
+reg [18:0] delayed_pcm_data [15:0];
+reg [5:0] mic_delays [15:0];  
+
+integer j;
+
+always @(posedge clk or posedge rst) begin
+  if (rst) begin 
+    for (j = 0; j < 16; j = j + 1) begin
+      pcm_data[j] = 19'b0;
+      delayed_pcm_data[j] = 19'b0;
+    end
+  end else begin
+    case (delay_select)
+      0 : mic_delays <= '{0, 4, 10, 12, 14, 18, 0, 0, 0, 0, 0, 0, 4, 10, 12, 14}
+    endcase
+  end
+end
+
+genvar i;
+
+generate
+  for (i = 0; i < 16; i = i + 1) begin
+   delay_line (
+    .clk(clk),
+    .rst(rst),
+    .delay(mic_delays[i]),
+    .pcm_data(pcm_data[i]),
+    .delayed_pcm_data(delayed_pcm_data[i])
+  );
+  end
+endgenerate
+
+endmodule
