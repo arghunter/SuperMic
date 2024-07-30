d06f91915f7bf5fc7fc1f032a19841120576f906
Sanjay Ravishankar <sanrav2016@gmail.com>
2024-07-30 15:41:57 -0400

    changes

--- delay/delay_module.v
+++ delay/delay_module.v
@@ -77,12 +77,7 @@ module delay_module (
     integer j;
 
     always @(posedge clk or posedge rst) begin
-      if (rst) begin 
-        for (j = 0; j < 16; j = j + 1) begin
-          pcm_data[j] <= 19'b0;
-          delayed_pcm_data[j] <= 19'b0;
-        end
-      end else begin
+      begin
         case (delay_select)
 
 	endcase
