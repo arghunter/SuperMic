3133926a728f3b81370198983441adcd8d906141
Asmi <107591325+asmisawant@users.noreply.github.com>
2024-07-30 07:59:07 -0700

    Update adder_16x23bi.v

--- adder/adder_16x23bi.v
+++ adder/adder_16x23bi.v
@@ -1,20 +1,36 @@
 module adder_16x23bit (
-    input [22:0] in [15:0],
+    input [18:0] in_0,
+    input [18:0] in_1,
+    input [18:0] in_2,
+    input [18:0] in_3,
+    input [18:0] in_4,
+    input [18:0] in_5,
+    input [18:0] in_6,
+    input [18:0] in_7,
+    input [18:0] in_8,
+    input [18:0] in_9,
+    input [18:0] in_10,
+    input [18:0] in_11,
+    input [18:0] in_12,
+    input [18:0] in_13,
+    input [18:0] in_14,
+    input [18:0] in_15,
     output [22:0] sum,
     output carry_out
 );
+
     wire [22:0] sum_intermediate [14:0];
     wire [14:0] carry_out_intermediate;
 
     // Stage 1: Add pairs of inputs
-    adder_23bit add0 ( .a(in[0]), .b(in[1]), .sum(sum_intermediate[0]), .carry_out(carry_out_intermediate[0]) );
-    adder_23bit add1 ( .a(in[2]), .b(in[3]), .sum(sum_intermediate[1]), .carry_out(carry_out_intermediate[1]) );
-    adder_23bit add2 ( .a(in[4]), .b(in[5]), .sum(sum_intermediate[2]), .carry_out(carry_out_intermediate[2]) );
-    adder_23bit add3 ( .a(in[6]), .b(in[7]), .sum(sum_intermediate[3]), .carry_out(carry_out_intermediate[3]) );
-    adder_23bit add4 ( .a(in[8]), .b(in[9]), .sum(sum_intermediate[4]), .carry_out(carry_out_intermediate[4]) );
-    adder_23bit add5 ( .a(in[10]), .b(in[11]), .sum(sum_intermediate[5]), .carry_out(carry_out_intermediate[5]) );
-    adder_23bit add6 ( .a(in[12]), .b(in[13]), .sum(sum_intermediate[6]), .carry_out(carry_out_intermediate[6]) );
-    adder_23bit add7 ( .a(in[14]), .b(in[15]), .sum(sum_intermediate[7]), .carry_out(carry_out_intermediate[7]) );
+    adder_23bit add0 ( .a(in_0), .b(in_1), .sum(sum_intermediate[0]), .carry_out(carry_out_intermediate[0]) );
+    adder_23bit add1 ( .a(in_2), .b(in_3), .sum(sum_intermediate[1]), .carry_out(carry_out_intermediate[1]) );
+    adder_23bit add2 ( .a(in_4), .b(in_5), .sum(sum_intermediate[2]), .carry_out(carry_out_intermediate[2]) );
+    adder_23bit add3 ( .a(in_6), .b(in_7), .sum(sum_intermediate[3]), .carry_out(carry_out_intermediate[3]) );
+    adder_23bit add4 ( .a(in_8), .b(in_9), .sum(sum_intermediate[4]), .carry_out(carry_out_intermediate[4]) );
+    adder_23bit add5 ( .a(in_10), .b(in_11), .sum(sum_intermediate[5]), .carry_out(carry_out_intermediate[5]) );
+    adder_23bit add6 ( .a(in_12), .b(in_13), .sum(sum_intermediate[6]), .carry_out(carry_out_intermediate[6]) );
+    adder_23bit add7 ( .a(in_14), .b(in_15), .sum(sum_intermediate[7]), .carry_out(carry_out_intermediate[7]) );
 
     // Stage 2: Add results of stage 1
     adder_23bit add8 ( .a(sum_intermediate[0]), .b(sum_intermediate[1]), .sum(sum_intermediate[8]), .carry_out(carry_out_intermediate[8]) );
@@ -32,4 +48,5 @@ module adder_16x23bit (
     // Final stage: Add the last intermediate sum to get the final result
     assign sum = sum_intermediate[14];
     assign carry_out = carry_out_intermediate[14];
+
 endmodule
