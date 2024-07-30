8a5f3707fa8f9c189553b5f21265ae192c4a3288
Morgan-Packard <143810907+Morgan-Packard@users.noreply.github.com>
2024-07-30 16:05:22 -0400

    little cleanup comb.v
    quick cleaning

--- cic/comb.v
+++ cic/comb.v
@@ -10,26 +10,14 @@ module comb (
     input wire [18:0] i_data;
     output reg [18:0] o_data;
 
-    integer i;
-
     reg [18:0] delay_line;
     reg [18:0] temp_delay;
-    //sign_extend = {(OW-IW){delay_line[0][IW-1]}, delay_line[0]}
-    //assign o_data = {{(OW-IW){i_data[IW-1]}}, i_data} - {{(OW-IW){delay_line[M-1][IW-1]}}, delay_line[M-1]};
 
-    always @(posedge lr_clock) begin
-        // for (i=1; i<N; i=i+1) begin
-        //     delay_line[i] <= delay_line[i-1];
-        // end
-        //i_data-last_input
-        // o_data <= {{(OW-IW){i_data[IW-1]}}, i_data} - {{(OW-IW){delay_line[N-1][IW-1]}}, delay_line[N-1]};
-        
+    always @(posedge lr_clock) begin    
         o_data <= i_data-delay_line;
         temp_delay <= i_data;
-        // delay_line[0] <= i_data;
     end 
     always @(negedge lr_clock) begin
-        
         delay_line <=temp_delay;
     end
 endmodule
